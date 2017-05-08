3297cd5f8ba8ac33bc730c09c6bdc19da8e93207
Gerard Rodes <grv94sk@gmail.com>
2017-05-08 10:37:40 +0200

    Now it edits headers

--- AutoTranslate.py
+++ AutoTranslate.py
@@ -4,10 +4,15 @@ import os
 import json
 import requests
 import urllib
+import codecs
+from datetime import datetime
+from babel import Locale
+
 
 
 def dump(data):
   print json.dumps(data, indent=2)
+
 
 
 class AutoTranslate(object):
@@ -16,14 +21,33 @@ class AutoTranslate(object):
               'trnsl.1.1.20170411T115549Z.0f958e65e97f0a7f.614338348957347456037644c2cc30b928e31658',
               'trnsl.1.1.20170411T115943Z.cfea43dbbcd66f79.1899e548da3d1d0b46f3e2f51aa1dc15d6110026']
   YANDEX_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-  
+  replaces = {
+    'ca': {
+      'l * l': 'l·l',
+    },
+  }
+  headers = {
+    'Project-Id-Version': None,
+    'PO-Revision-Date': None,
+    'Last-Translator': 'AUTO TRANSLATOR <grodes@semic.es>',
+    'Language-Team': 'YANDEX',
+    'MIME-Version': '1.0',
+    'Content-Type': 'text/plain; charset=utf-8',
+    'Content-Transfer-Encoding': '8bit',
+    'Plural-Forms': 'nplurals=1; plural=0',
+    'Language-Code': None,
+    'Language-Name': None,
+    'Preferred-Encodings': 'utf-8 latin1',
+    'Domain': None,
+  }
   def __init__(self, domain, source_language='en', locales_path = '.', overwrite = False):
     self.domain = domain
     self.source_language = source_language
     self.locales_path = locales_path
     self.overwrite = overwrite
-    
     self.files = []
+    self.headers['Project-Id-Version'] = domain
+    self.headers['Domain'] = domain
     
     
   def __call__(self):
@@ -40,18 +64,25 @@ class AutoTranslate(object):
   def parse_files(self):
     for language in self.languages:
       file_info = {
-        'language' : language,
+        'headers': dict(self.headers),
+        'language': language,
         'file_path': '/'.join( (self.locales_path, language, 'LC_MESSAGES', self.domain + '.po') ),
         'lines': [],
         'translations': []
       }
-      
+
+      file_info['headers']['Language-Code'] = language
+      file_info['headers']['Language-Name'] = Locale(language).display_name.title()
+      file_info['headers']['PO-Revision-Date'] = datetime.now().strftime('%Y-%m-%d %H:%M %z')
+
       with open(file_info['file_path'], 'rU') as file:
-        file_info['lines'] = [line for line in file.readlines()]
+        file_info['lines'] = [line if isinstance(line, unicode) else line.decode('utf-8') for line in file.readlines()]
         
         for i, line in enumerate(file_info['lines']):
           
           if line.startswith('msgid'):
+            # Parsing translations
+
             msgid = line.strip('\n')[7:-1]
             next_line = file_info['lines'][i+1]
             
@@ -63,12 +94,12 @@ class AutoTranslate(object):
                     'msgstr': msgstr,
                     'line'  : i+1,
                   })
+
+
       
       self.files.append(file_info)
 
 
-      
-      
   def translate_files(self):
     api_key = 0
     
@@ -88,8 +119,7 @@ class AutoTranslate(object):
             data['text'] = translation['msgid']
             r = requests.post(self.YANDEX_URL, data=data)
             response = r.json()
-
-            translation['msgstr'] = response['text'][0]
+            translation['msgstr'] = self.replace_yandex_errors(file['language'], response['text'][0])
 
             if response['code'] == 200:
               success = True
@@ -111,28 +141,41 @@ class AutoTranslate(object):
         print 'nothing no translate, if you want to overwrite existing translation set overwrite parameter to True'
 
 
-
   def rebuild_files(self):
     for file in self.files:
       modified_file = file['lines']
 
+      # Write new headers
+      for index, line in enumerate(modified_file):
+        if line.startswith('"'):
+          attribute, value = line.replace('"', '').replace('\\n', '').rstrip().split(': ')
+          if file['headers'].get(attribute, False):
+            modified_file[index] = u'"{attribute}: {value}\\n"\n'.format(attribute=attribute, value=file['headers'][attribute])
+
+      # Write translations
       if file['translations']:
         for translation in file['translations']:
           line_to_edit = int(translation['line'])
           new_line_str = 'msgstr "%s"\n' % translation['msgstr']
-
           modified_file[line_to_edit] = new_line_str
-
         
         new_file_path = '/'.join( (self.locales_path, file['language'], 'LC_MESSAGES', self.domain + '.new.po') )
 
-        with open(new_file_path, 'w') as new_file:
-          new_file.write( u''.join(modified_file).encode('utf-8') )
+        with codecs.open(new_file_path, "w", encoding="utf-8") as new_file:
+          text = u''.join(modified_file)
+          new_file.write(text)
 
         if os.path.exists(new_file_path):
           os.remove(file['file_path'])
           os.rename(new_file_path, file['file_path'])
 
+
+  def replace_yandex_errors(self, lang, string):
+    if self.replaces.get(lang, None):
+      for replace in self.replaces[lang]:
+        string = string.replace(replace.decode('utf-8'), self.replaces[lang][replace].decode('utf-8'))
+
+    return string
             
         
         
